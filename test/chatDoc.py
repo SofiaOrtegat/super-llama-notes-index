8288cdb1fa6928e43f257625c8c736d5684f998a
SofiaOrtegat <SofiaOrtegat@proton.me>
2022-11-13 18:36:34 +0000

    update doc strings

--- test/chatDoc.py
+++ test/chatDoc.py
@@ -14,4 +14,4 @@ from llama_index import (
 
 # configuring LLM
 from llama_index.llm_predictor import HuggingFaceLLMPredictor
-stable_llm_predictor = HuggingFaceLLMPre
+stable_llm_predictor = HuggingFaceLLMPredictor
