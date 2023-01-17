b94fcf5045cf2ed78d44a454519a02fcb54b110d
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-01-17 12:36:07 +0000

    initial fork

--- test/chatDoc.py
+++ test/chatDoc.py
@@ -18,4 +18,4 @@ stable_llm_predictor = HuggingFaceLLMPredictor(
     tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
     model_name="StabilityAI/stablelm-tuned-alpha-3b"
 )
-service_context = ServiceContext
+service_context = ServiceContext.fr
