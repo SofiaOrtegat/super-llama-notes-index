87403186d2aeacb401525107eeef1c7b1948b6c0
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-04-22 10:13:05 +0000

    Bump version: 0.1.3 → 0.1.4

--- test/chatDoc.py
+++ test/chatDoc.py
@@ -23,4 +23,4 @@ service_context = ServiceContext.from_defaults(
     llm_predictor=stable_llm_predictor
 )
 HuggingFaceLLMPredictor(
-    tokenizer_outputs_to_remove=["token_type_id
+    tokenizer_outputs_to_remove=["token_type_ids"]
