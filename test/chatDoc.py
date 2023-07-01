adaaf2a7611399755a935bc96654e903a7f8ed08
SofiaOrtegat <SofiaOrtegat@proton.me>
2023-07-01 10:11:06 +0000

    Many small fixes

--- test/chatDoc.py
+++ test/chatDoc.py
@@ -29,4 +29,4 @@ HuggingFaceLLMPredictor(
 # main
 documents = SimpleDirectoryReader('../data').load_data()
 parser = node_parser.SimpleNodeParser()
-nodes = parser.get_nodes_from_documents(documents=docu
+nodes = parser.get_nodes_from_documents(documents=documen
